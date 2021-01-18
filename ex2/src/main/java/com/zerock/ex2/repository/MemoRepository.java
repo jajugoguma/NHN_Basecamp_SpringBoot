8b55c005e0a3b958ba737f10883ef9fbec0d06a2
jajugoguma <jajugoguma@gmail.com>
2021-01-18 18:22:54 +0900

    Add ex1, ex2

---
+++ ex2/src/main/java/com/zerock/ex2/repository/MemoRepository.java
@@ -0,0 +1,8 @@
+package com.zerock.ex2.repository;
+
+import com.zerock.ex2.entity.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+}
