0da6600dbf2aa6c40dfc9520e870af12dd39b059
jajugoguma <jajugoguma@gmail.com>
2021-01-19 10:50:01 +0900

    Add ex3

---
+++ ex3/src/main/java/com/zerock/ex3/dto/SampleDTO.java
@@ -0,0 +1,18 @@
+package com.zerock.ex3.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class SampleDTO {
+    private Long sno;
+
+    private String first;
+
+    private String last;
+
+    private LocalDateTime regTime;
+}
