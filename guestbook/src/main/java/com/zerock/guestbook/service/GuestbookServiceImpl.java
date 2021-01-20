772f27d005e457e447553c7a878bc2e3b8f96202
jajugoguma <jajugoguma@gmail.com>
2021-01-20 11:05:12 +0900

    서비스 계층 목록처리 완(인터페이스 및 구현, 테스트) Page 내 페이지 번호 출력 및 링크 구현, Previous, Next 버튼 구현

---
+++ guestbook/src/main/java/com/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -0,0 +1,50 @@
+package com.zerock.guestbook.service;
+
+import com.zerock.guestbook.dto.GuestbookDTO;
+import com.zerock.guestbook.dto.PageRequestDTO;
+import com.zerock.guestbook.dto.PageResultDTO;
+import com.zerock.guestbook.entity.Guestbook;
+import com.zerock.guestbook.repository.GuestbookRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class GuestbookServiceImpl implements GuestbookService{
+
+    private final GuestbookRepository repository;
+
+    @Override
+    public Long register(GuestbookDTO dto) {
+
+        log.info("DTO-----------------------------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+
+        log.info(entity);
+
+        repository.save(entity);
+
+        return entity.getGno();
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+
+        Page<Guestbook> result = repository.findAll(pageable);
+
+        Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
+
+        return new PageResultDTO<>(result, fn);
+    }
+}
